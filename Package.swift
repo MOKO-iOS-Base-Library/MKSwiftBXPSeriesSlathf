a2cff78c466416092404893a1d7d2047cd1ee760
lovexiaoxia <aadyx2007@163.com>
2025-08-26 15:53:59 +0800

    Fix

--- Package.swift
+++ Package.swift
@@ -1,49 +1,39 @@
 // swift-tools-version: 6.0
-// The swift-tools-version declares the minimum version of Swift required to build this package.
-
 import PackageDescription
 
 let package = Package(
     name: "MKSwiftBXPSeriesSlathf",
-    platforms: [
-        .iOS(.v14),
-    ],
+    platforms: [.iOS(.v14)],
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "MKSwiftBXPSeriesSlathf",
-            targets: ["MKSwiftBXPSeriesSlathf"]),
+            targets: ["MKSwiftBXPSeriesSlathf"]
+        ),
     ],
     dependencies: [
-        .package(url: "https://github.com/MOKO-iOS-Base-Library/MKBaseSwiftModule.git", .upToNextMajor(from: "1.0.14")),
-        .package(url: "https://github.com/MOKO-iOS-Base-Library/MKSwiftBeaconXCustomUI.git", .upToNextMajor(from: "1.0.10")),
-        .package(url: "https://github.com/MOKO-iOS-Base-Library/MKSwiftBleModule.git", .upToNextMajor(from: "1.0.12")),
-        .package(url: "https://github.com/itsmeichigo/DateTimePicker.git", .upToNextMajor(from: "2.5.3"))
+        .package(url: "https://github.com/MOKO-iOS-Base-Library/MKBaseSwiftModule.git", from: "1.0.14"),
+        .package(url: "https://github.com/MOKO-iOS-Base-Library/MKSwiftBeaconXCustomUI.git", from: "1.0.10"),
+        .package(url: "https://github.com/MOKO-iOS-Base-Library/MKSwiftBleModule.git", from: "1.0.12"),
+        .package(url: "https://github.com/itsmeichigo/DateTimePicker.git", from: "2.5.3")
     ],
     targets: [
         .target(
             name: "MKSwiftBXPSeriesSlathf",
             dependencies: [
-                .product(name: "MKBaseSwiftModule", package: "MKBaseSwiftModule"),
-                .product(name: "MKSwiftBeaconXCustomUI", package: "MKSwiftBeaconXCustomUI"),
-                .product(name: "MKSwiftBleModule", package: "MKSwiftBleModule"),
-                .product(name: "DateTimePicker", package: "DateTimePicker")
+                "MKBaseSwiftModule",
+                "MKSwiftBeaconXCustomUI",
+                "MKSwiftBleModule",
+                "DateTimePicker"
             ],
             path: "Sources",
-            resources: [
-                .process("Assets")
-            ],
+            resources: [.process("Resources")],
             swiftSettings: [
-                .define("DEBUG", .when(configuration: .debug)),
-                .define("IOS14_OR_LATER")  // 添加编译标志
-            ],
-            linkerSettings: [
-                .linkedLibrary("z"),
-                .linkedLibrary("iconv")
+                .define("DEBUG", .when(configuration: .debug))
             ]
         ),
         .testTarget(
             name: "MKSwiftBXPSeriesSlathfTests",
-            dependencies: ["MKSwiftBXPSeriesSlathf"])
+            dependencies: ["MKSwiftBXPSeriesSlathf"]
+        )
     ]
 )
